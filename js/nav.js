820533ef8fe7c68d37fbd771ac769837ee7d5e54
spudtrader <jmurphy.web@gmail.com>
2016-01-25 07:17:26 +0000

    attempted to fix splash background image

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
+//add a moving menu bar
 document.getElementById('hamburger-button').addEventListener('click', function(event){
 
     console.log(document.getElementsByClassName('ul-nav-menu'));
     document.getElementsByClassName('ul-nav-menu')[0].classList.toggle('show');
-
+    
 });
