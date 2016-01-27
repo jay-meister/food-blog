acd69319e780923971ed45e597ff825c9f65aed1
spudtrader <jmurphy.web@gmail.com>
2016-01-27 08:08:32 +0000

    nav-bar positions
    resolves #4 and resolves #3

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,10 @@
-//add a moving menu bar
-document.getElementById('hamburger-button').addEventListener('click', function(event){
 
-    console.log(document.getElementsByClassName('ul-nav-menu'));
+
+//add a moving menu bar
+var toggle = false;
+document.getElementById('hamburger-button').addEventListener('click', function(event){
+    //toggle class .show
     document.getElementsByClassName('ul-nav-menu')[0].classList.toggle('show');
-    
+    //toggle image for hamburger-button
+
 });
