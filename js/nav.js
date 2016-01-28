7eaaa784589173d286ea767ba0d9d9ad6628888e
spudtrader <jmurphy.web@gmail.com>
2016-01-28 08:33:19 +0000

    animated hamburger

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,9 @@
 
 //add a moving menu bar
 var toggle = false;
-document.getElementById('hamburger-button').addEventListener('click', function(event){
-    //toggle class .show
-    document.getElementsByClassName('ul-nav-menu')[0].classList.toggle('show');
-    //toggle image for hamburger-button
 
+
+document.getElementById('nav-icon4').addEventListener('click', function(event){
+    this.classList.toggle('open');
+    document.getElementsByClassName('ul-nav-menu')[0].classList.toggle('show');
 });
